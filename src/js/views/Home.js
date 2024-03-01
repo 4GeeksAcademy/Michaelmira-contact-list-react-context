58059db454176ea333950021f6ab14183df402d0
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-03-01 06:04:29 +0000

    Completed Flux.js for now, and Home is now linked and workign with all Complete

--- src/js/views/Home.js
+++ src/js/views/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useState } from "react";
 // import "../../styles/home.css";
 import { Link } from "react-router-dom";
@@ -8,27 +8,7 @@ import { ContactCard } from "../component/ContactCard";
 
 export const Home = () => {
 
-	const [ contacts, setContacts] = useState ([])
-	const [ contactToUpdate, setContactToUpdate] = useState (null)
-		
-	const addContact =(contact) => {
-		console.log ("This is the contact", contact);
-		setContacts(contacts.concat(contact))
-	};
-
-	const updateContact =(updatedContact) => {
-		console.log ("Contact To Update", contact);
-		setContacts(contacts.map((oldContact, index) => {
-			if(contactToUpdate.index === index ) return updatedcontact;
-			else return oldContact;
-		}));
-		setContactToUpdate(null);
-	};
-
-	const deleteContact =(indexToDelete) => {
-		console.log ("We are deleteing");
-		setContacts(contacts.filter((item, index) => indexToDelete !== index  ));
-	};
+	const {store, actions } = useContext(Context)
 
 	return (
 
@@ -36,9 +16,7 @@ export const Home = () => {
 			<div className="container">
 				<div>
 					<div className="text-right my-3">
-						<AddContact 
-						onSave={contactToUpdate ? updateContact : addContact} 
-						contact={contactToUpdate}  />
+
 					<Link className="btn btn-success" to="/add-contact" >
 						Please press this button to Add Contact
 					</Link>
@@ -48,14 +26,11 @@ export const Home = () => {
 						className="panel-collapse collapse show"
 						aria-expanded="true">
 						<ul className="list-group pull-down" id="contact-list">
-							{contacts.map((c, i) => (
-							  	<ContactCard 
-									contact={c} 
-									onDelete={() => deleteContact(i)}
-									onUpdate={() => 
-										setContactToUpdate({...c, index: i })
-									} 
-								/>
+							{store.contacts.map((c, i) => (
+								<div>{c?.full_name}</div>
+							  	// <ContactCard 
+								// 	contact={c} 
+								// />
 							))}
 						</ul>
 					</div>

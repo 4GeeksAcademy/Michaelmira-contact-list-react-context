ea31171e0da19ff79c0711ef9a4f658e59d35e46
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-03-04 01:19:01 +0000

    move all contact card info to far left

--- src/js/component/ContactCard.js
+++ src/js/component/ContactCard.js
@@ -42,26 +42,35 @@ export const ContactCard = ({contact}) => {
 						</div>
 					</div>	
 					<br />
-					<i className="fas fa-map-marker-alt text-muted mr-3"></i>
-					<span className="text-muted">
-						{contact.address}
-						</span>
-					<br />
-					<span
-						className="fa fa-phone fa-fw text-muted mr-3"
-						data-toggle="tooltip"
-						title=""
-						data-original-title="(870) 288-4149"></span>
-					<span className="text-muted small">{contact.phone}</span>
-					<br />
-					<span
-						className="fa fa-envelope fa-fw text-muted mr-3"
-						data-toggle="tooltip"
-						data-original-title=""
-						title=""></span>
-					<span className="text-muted small text-truncate">
-						{contact.email}
-					</span>
+					<div className="d-flex flex-column align-items-start">
+						<div>
+							<i className="fas fa-map-marker-alt text-muted mr-3"></i>
+							<span className="text-muted">
+								{contact.address}
+							</span>
+						</div>
+						
+						<div>
+							<span
+								className="fa fa-phone fa-fw text-muted mr-3"
+								data-toggle="tooltip"
+								title=""
+								data-original-title="(870) 288-4149"></span>
+							<span className="text-muted small">{contact.phone}
+							</span>
+						</div>
+						
+						<div>
+							<span
+								className="fa fa-envelope fa-fw text-muted mr-3"
+								data-toggle="tooltip"
+								data-original-title=""
+								title=""></span>
+							<span className="text-muted small text-truncate">
+								{contact.email}
+							</span>
+						</div>
+					</div>
 				</div>
 			</div>
 		</li>

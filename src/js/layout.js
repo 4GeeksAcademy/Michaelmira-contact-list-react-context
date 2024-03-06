5e07197ea02503b41d0759c7a71c2a2092330d50
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-03-06 23:27:40 +0000

    finish

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./views/Home";
 import injectContext from "./store/appContext";
@@ -18,14 +17,12 @@ const Layout = () => {
 	return (
 		<div>
 			<BrowserRouter basename={basename}>
-				<ScrollToTop>
 					<Routes>
 						<Route path="/" element={<Home/>} />
 						<Route path="/add-contact" element={<AddContact />} />
 						<Route path="/edit-contact/:theid" element={<EditContact />} />
 						<Route path="*" element={<h1>Not found!</h1>} />
 					</Routes>
-				</ScrollToTop>
 			</BrowserRouter>
 		</div>
 	);
